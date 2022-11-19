836cd2109429b2f132944de328ce369ae7e25f10
Shradha_Andhare <73115655+shradha09git@users.noreply.github.com>
2022-11-19 15:22:34 +0530

    Update SubHeading.js

--- src/components/SubHeading.js
+++ src/components/SubHeading.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export function SubHeading() {
+function SubHeading() {
     return (
         <h5>Happy to solve you doubts.</h5>
     )
 }
+
+export default SubHeading
